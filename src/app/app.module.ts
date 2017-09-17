a08e18b2d676d34f53553f082e72fcb1a26ac9a9
jacksmam <jacksmam0@gmail.com>
2017-09-17 22:26:44 +0900

    slide-listのコンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideListComponent } from './slide-list/slide-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideListComponent
   ],
   imports: [
     BrowserModule,
